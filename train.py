ddbf67a844d49c977c54c9671b490d59d6db08b3
AliBenoua <142593954+AliBenoua@users.noreply.github.com>
2025-08-19 12:10:56 +0100

    Update train.py

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ model = YOLO("yolo11n.yaml")
 
 
 
-results = model.train(task="segment",data="yourdataset/data.yaml",device = [0,1],
+results = model.train(task="detect",data="yourdataset/data.yaml",device = [0,1],
                                                                               epochs=150,
                                                                               patience=150,
                                                                               batch=8,
